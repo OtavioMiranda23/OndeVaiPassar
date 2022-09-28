80899c7caaf923c2e70eeade1a59a584403b5a24
OtavioMiranda23 <otaviomiranda@usp.br>
2022-09-28 15:18:54 -0300

    adiciona typescript

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,4 @@
+import {Request, Response} from 'express';
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -7,11 +8,13 @@ const app = express();
 const jsonTransmissoes = require('../jsonScrap/Json_Transmissoes.json');
 const { json, response } = require('express');
 
+
+
 const db = mySql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'transmissoes'
+    database: 'transmissoesserieb'
 })
 
 app.use(cors());
@@ -19,26 +22,24 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-
-app.get("/api/get", (req, res) => {
+app.get("/api/get", (req: Request, res: Response) => {
     //Pega transmissoes do banco de dados
     const sqlSelect = "SELECT * FROM tablepartidas";
-    db.query(sqlSelect, (err, result) => {
+    db.query(sqlSelect, (err: Error, result: String ) => {
         res.send(result)
     })
 })
 
-app.post("/api/insert", (req, res) => {
+app.post("/api/insert", (req: Request, res: Response) => {
     //Insere informações no banco de dados
 
     for (const propriedade in jsonTransmissoes) {
         //Chave => propriedade; Valor => json[propriedade]
         let confronto = propriedade;
         let transmissao = jsonTransmissoes[propriedade];
-        const sqlInsert = "INSERT INTO tablepartidas (Equipes, Transmissao) VALUES (?, ?)";
+        const sqlInsert = "INSERT INTO table_serie_b (Equipes, Transmissao) VALUES (?, ?)";
 
-        db.query(sqlInsert, [confronto, transmissao], (err, result) => {
+        db.query(sqlInsert, [confronto, transmissao], (err: Error, result: String) => {
             console.log(result);
         })
     }
@@ -46,11 +47,11 @@ app.post("/api/insert", (req, res) => {
 
 })
 
-app.delete("/api/delete", (req, res) => {
+app.delete("/api/delete", (req: Request, res: Response) => {
     //Deleta informações no banco de dados
 })
 
-app.put("/api/update", (req, res) => {
+app.put("/api/update", (req: Request, res: Response) => {
     //Atualiza informações no banco de dados
 })
 
